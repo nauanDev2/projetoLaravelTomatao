1a69cc10b425d21790afc3d4ac4f0d7247d27e35
NauanDev2 <nauan.dev2@gmail.com>
2025-03-18 20:11:35 -0300

    drinho do dry

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,7 @@
+
+<h3>Contato</h3>
+<ul>    
+    <li><a href="/">Principal</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobreNos">Sobre-Nos</a></li>
+</ul>    
---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,7 @@
+
+    <h3>Principal</h3>
+    <ul>    
+        <li><a href="/">Principal</a></li>
+        <li><a href="/contato">Contato</a></li>
+        <li><a href="/sobreNos">Sobre-Nos</a></li>
+    </ul>    
